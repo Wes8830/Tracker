dc28f45902b8f9fdb95f77c5e29ec3e7987f86df
Wes8830 <wes.vesprini@gmail.com>
2022-02-06 11:37:26 -0500

    'removing files that were moved to /src directory'

--- tracker/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-from .models import Client, Product, Component, UseCase, UserStory, Product_UseCase, Product_Component, Client_Product, Client_UseCase
-
-# Create your tests here.
-class ModelsTestCase(TestCase):
-
-    def createClient(self):
-        return false
